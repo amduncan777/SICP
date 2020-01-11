ffcf0c72d2a7073a961f8859132cbce7ab4a18e3
amduncan777 <44075335+amduncan777@users.noreply.github.com>
2020-01-11 13:07:32 -0500

    Update ex-2.5.scm

--- ex-2.5.scm
+++ ex-2.5.scm
@@ -3,5 +3,15 @@
      (expt 3 b)))
      
 (define (car z)
-  (/ z
-     (* 
+  (define (iter z i)
+    (if (= (remainder z 2) 0)
+        (iter (/  z 2) (+ i 1))
+        i
+  (iter z 0)
+        
+(define (cdr z)
+  (define (iter z i)
+    (if (= (remainder z 3) 0)
+        (iter (/  z 3) (+ i 1))
+        i
+  (iter z 0)
