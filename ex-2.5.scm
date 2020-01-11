49794f0798fe4fc1269bb8a06c22e77b74fbddd4
amduncan777 <44075335+amduncan777@users.noreply.github.com>
2020-01-11 13:12:10 -0500

    Update ex-2.5.scm

--- ex-2.5.scm
+++ ex-2.5.scm
@@ -6,12 +6,12 @@
   (define (iter z i)
     (if (= (remainder z 2) 0)
         (iter (/  z 2) (+ i 1))
-        i
-  (iter z 0)
+        i))
+  (iter z 0))
         
 (define (cdr z)
   (define (iter z i)
     (if (= (remainder z 3) 0)
         (iter (/  z 3) (+ i 1))
-        i
-  (iter z 0)
+        i))
+  (iter z 0))
